65294ebccfb99bce8c7d75cd7d00b891dd8e6196
o2redshift <44179303+HYang1996@users.noreply.github.com>
2020-11-22 23:16:37 +0000

    updates

--- setup.py
+++ setup.py
@@ -5,13 +5,7 @@ setuptools.setup(
     version="0.0.1",
     license='MIT',
     author="Cheuk Ming Chung, Hongyi Yang",
-    author_email="author@example.com",
     url="https://github.com/HYang1996/NetworkSim",
     packages=setuptools.find_packages(),
-    python_requires='>=3.6',
-    install_requires=[
-        'numpy',
-        'simpy',
-        'pandas'
-      ],
+    python_requires='>=3.6'
 )
